cce845e3b1b4f129b0433375a9dec9704f0b36ae
shreekumarsiltu <shreekumarsundaray1999@gmail.com>
2019-12-20 09:20:35 +0530

    update

--- demo.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-#include<ctype.h>
-void main(){
-	char ch = '+';
-	printf("%d",1ch2);
-	getch();
-}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include<conio.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<math.h>
-
-void main(){
-	string s = "ahb b";
-	printf("%d",s.length());
-}
